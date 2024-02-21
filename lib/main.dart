8ffcd46312ee5555210d1a2925d3a7d715a691f0
JoeCheng <cycheng@fmt.com.tw>
2024-02-21 10:44:04 +0800

    加fav按鈕

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        ),
-        home: MyHomePage(),
-      ),
-    );
+	return ChangeNotifierProvider(
+		create: (context) => MyAppState(),
+		child: MaterialApp(
+		title: 'Namer App',
+		theme: ThemeData(
+			useMaterial3: true,
+			colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+		),
+		home: MyHomePage(),
+		),
+	);
   }
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+	var current = WordPair.random();
 
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
+	void getNext() {
+		current = WordPair.random();
+		notifyListeners();
+	}
+
+  	var favorites = <WordPair>[];
+	
+	void toggleFavorite() {
+		if (favorites.contains(current)) {
+			favorites.remove(current);
+		} else {
+			favorites.add(current);
+		}
+		notifyListeners();
+	}
+
 }
 
 class MyHomePage extends StatelessWidget {
@@ -40,6 +52,14 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+	// ↓ Add this.
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -47,7 +67,27 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 10),
-            ElevatedButton(onPressed: () { appState.getNext(); }, child: Text("Next"))
+            Row(
+				mainAxisSize: MainAxisSize.min,
+        		children: [
+					ElevatedButton.icon(
+						onPressed: () {
+						appState.toggleFavorite();
+						},
+						icon: Icon(icon),
+						label: Text("Like")
+					),	
+
+					SizedBox(width: 10),
+					
+					ElevatedButton(
+						onPressed: () {
+						appState.getNext();
+						},
+						child: Text("Next")
+					),
+              	],
+            )
           ],
         ),
       ),
@@ -76,9 +116,9 @@ class BigCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Text(
-            pair.asLowerCase,
-            style: style,
-            semanticsLabel: "${pair.first} ${pair.second}",
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
     );
